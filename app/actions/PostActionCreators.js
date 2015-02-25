a92f45adb36a0cd09ec47b6035abfdd037bb90a6
Christian de Botton <cdebotton@gmail.com>
2015-02-25 08:20:48 -0500

    Wiring up get post actions.

--- app/actions/PostActionCreators.js
+++ app/actions/PostActionCreators.js
@@ -1,12 +1,42 @@
 "use strict";
 
 import alt from "../alt";
+import invariant from "react/lib/invariant";
+import TumblrAPI from "../utils/TumblrAPI";
 
 class PostActionCreators {
   constructor() {
     this.generateActions(
-      'getPosts'
+      'getPostsSuccess',
+      'getPostsError',
+      'getPostSuccess',
+      'getPostError'
     );
+  }
+
+  getPosts() {
+    this.dispatch();
+
+    return TumblrAPI.getPosts()
+      .then(this.actions.getPostsSuccess)
+      .catch(this.actions.getPostsError);
+  }
+
+  getPost(id = null) {
+    let type = Object.prototype.toString.call(parseInt(id));
+
+    invariant(
+      type === '[object Number]',
+      'PostActionCreators.getPost(...): Please provide a valid number id. ' +
+      'Received `%s`',
+      type
+    );
+
+    this.dispatch(id);
+
+    return TumblrAPI.getPost(id)
+      .then(this.actions.getPostSuccess)
+      .catch(this.actions.getPostError);
   }
 }
 
