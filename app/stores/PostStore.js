44e5619d0623a5af6f0ac7e58c604f63a0dea326
Christian de Botton <cdebotton@gmail.com>
2015-02-25 08:27:03 -0500

    Starting post store.

--- app/stores/PostStore.js
+++ app/stores/PostStore.js
@@ -1,9 +1,48 @@
 "use strict";
 
 import alt from "../alt";
+import { OrderedMap, fromJS } from "immutable";
+import PostActionCreators from "../actions/PostActionCreators";
 
 class PostStore {
   constructor() {
+    this.bindActions(PostActionCreators);
+
+    this.on('init', setup);
+    this.on('bootstrap', setup);
+
+    this.posts = OrderedMap();
+  }
+
+  setup() {
+    if (! OrderedMap.isOrderedMap(this.posts)) {
+      this.posts = fromJS(this.posts, (key, value) => {
+        return OrderedMap(value);
+      });
+    }
+  }
+
+  onGetPosts() {
+
+  }
+
+  onGetPostsSuccess(resp = {}) {
+    let { posts } = resp.entities;
+
+    this.posts = this.posts.merge(posts);
+  }
+
+  onGetPostsError(err = null) {
+
+  }
+
+  onGetPostSuccess(resp = {}) {
+    let { posts } = resp.entities;
+
+    this.posts = this.posts.merge(posts);
+  }
+
+  onGetPostError(err = null) {
 
   }
 }
