c7f0cb17083bf8e534f07a1bca6a81cbc1346cc0
Christian de Botton <debotton@brooklynunited.com>
2015-03-03 11:28:20 -0500

    Working on photoset_layout comprehension.

--- app/components/Post.js
+++ app/components/Post.js
@@ -13,39 +13,60 @@ let Post = React.createClass({
     post: PropTypes.object.isRequired
   },
 
-  renderPhoto(photo, key) {
-    let { url } = photo.alt_sizes[0];
-    let style = {
-      backgroundImage: `url(${url})`
-    };
+  renderPhoto(opts) {
+    const { rows, sizes } = opts;
 
-    return (
-      <li
-        className="photo"
-        key={key}
-        style={style} />
-    );
+    return (photo, key) => {
+      let { url } = photo.alt_sizes[0];
+      let size = sizes[key] || 1;
+
+      let style = {
+        backgroundImage: `url(${url})`,
+        width: `${(1 / size) * 100}%`,
+        height: `${(1 / rows.length) * 100}%`,
+        float: "left"
+      };
+
+      return (
+        <li
+          className="photo"
+          key={key}
+          style={style} />
+      );
+    };
   },
 
   render() {
     let { post: rawPost }= this.props;
     let post = rawPost.toJS();
+    let layout = this.props.post.get("photoset_layout");
+    let rows = layout ? layout.split("").map(row => parseInt(row, 10)) : [1];
+    let sizes = expandSizes(rows);
+
+    let style = { paddingBottom: `${rows.length * 48}%` };
 
     return (
-      <div className="post">
+      <div style={style} className="post">
         { post.photos &&
           <ul className="photos">
-            {post.photos.map(this.renderPhoto)}
+            { post.photos.map(this.renderPhoto({ rows, sizes })) }
           </ul>
         }
         { post.tags && post.tags.length > 0 &&
-          <ul className="tags">
-
-          </ul>
+          <TagList tags={ post.tags } />
         }
       </div>
     );
   }
 });
+
+let expandSizes = (rows) => rows.reduce((memo, size) => {
+  let count = parseInt(size, 10);
+  while (count > 0) {
+    count--;
+    memo.push(parseInt(size, 10));
+  }
+  return memo;
+}, []);
 
 export default Post;
--- app/components/TagList.js
+++ app/components/TagList.js
@@ -2,15 +2,22 @@
 
 import React from "react/addons";
 
+const { PropTypes } = React;
 const { PureRenderMixin } = React.addons;
 
 let TagList = React.createClass({
   mixins: [PureRenderMixin],
 
+  propTypes: {
+    tags: PropTypes.arrayOf(PropTypes.string)
+  },
+
   render() {
+    let { tags } = this.props;
+
     return (
       <ul className="tag-list">
-
+        { tags.map((tag, key) => <li key={ key }>{ tag }</li>) }
       </ul>
     );
   }
