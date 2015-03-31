da6b44f6ba319c1e0495fc58c13099d554ba6940
Christian de Botton <cdebotton@gmail.com>
2015-03-31 00:47:52 -0400

    Finish quote.

--- app/components/Post/PostQuote.js
+++ app/components/Post/PostQuote.js
@@ -1,13 +1,27 @@
 "use strict";
 
 import React, { PropTypes } from "react/addons";
+import TitleBox from "./TitleBox";
+
+const { PureRenderMixin } = React.addons;
 
 class PostQuote extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return PureRenderMixin.shouldComponentUpdate
+      .call(this, nextProps, nextState);
+  }
+
   render() {
-    const { className } = this.props;
+    const { className, post } = this.props;
 
     return (
-      <div className={ className } />
+      <div className={ className }>
+        <quote dangerouslySetInnerHTML={{ __html: post.get("text") }} />
+        { post.has("source") &&
+          <span className="source">{ post.get("source") }</span>
+        }
+        <TitleBox post={ post } />
+      </div>
     );
   }
 }
