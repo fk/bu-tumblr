c15a45a9faa2851d2ec303cbec4de972a828dea3
Christian de Botton <cdebotton@gmail.com>
2015-03-31 00:55:45 -0400

    Adding video.

--- app/components/Post/PostVideo.js
+++ app/components/Post/PostVideo.js
@@ -1,13 +1,27 @@
 "use strict";
 
 import React, { PropTypes } from "react/addons";
+import TitleBox from "./TitleBox";
+
+const { PureRenderMixin } = React.addons;
 
 class PostVideo extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return PureRenderMixin.shouldComponentUpdate
+      .call(this, nextProps, nextState);
+  }
+
   render() {
-    const { className } = this.props;
+    const { className, post } = this.props;
+    let embed = post.getIn(["player", 2, "embed_code"]);
 
     return (
-      <div className={ className } />
+      <div className={ className }>
+        <div
+          className="player"
+          dangerouslySetInnerHTML={{ __html: embed }} />
+        <TitleBox post={ post } />
+      </div>
     );
   }
 }
