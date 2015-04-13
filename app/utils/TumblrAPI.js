8a596c5b00510f50d22bfe25dd7b875763b09687
Christian de Botton <debotton@brooklynunited.com>
2015-04-13 11:01:44 -0400

    Fix photo scaling.

--- app/utils/TumblrAPI.js
+++ app/utils/TumblrAPI.js
@@ -9,8 +9,8 @@ const Post = new Schema("posts");
 const Author = new Schema("authors");
 
 const ENDPOINT_ROOT = "http://api.tumblr.com/v2/blog";
-const TUMBLR = "flux-api.tumblr.com";
-const API_KEY = "fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4";
+const TUMBLR = "cbotzzz.tumblr.com";
+const API_KEY = "iiXyRVDvsjEMOJV1Ick83wXNir0GsHsH3NbIctuRjbPvVjq65Q";
 
 export default {
   async getPosts(params = {}) {
