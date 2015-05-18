0a8c84509a5a9e6f03b4d114532fe66da0012261
Christian de Botton <debotton@brooklynunited.com>
2015-05-18 11:15:57 -0400

    Require babel polyfill.

--- app/decorators/autobind.js
+++ app/decorators/autobind.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require("babel/polyfill");
+
 const autobind = (target, key, descriptor) => {
   let { name: componentName } = target.constructor;
   let { value: fn } = descriptor;
