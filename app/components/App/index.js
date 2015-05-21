891a6f5445e0f0601db1ee00e6f44aebe0f3ad4e
Christian de Botton <debotton@brooklynunited.com>
2015-05-21 14:50:20 -0400

    Style ul in text post.

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -5,6 +5,7 @@ import { RouteHandler } from "react-router";
 import classNames from "classnames";
 import Header from "../Header";
 import Footer from "../Footer";
+import Loader from "../Loader";
 import Navigation from "../Navigation";
 import Lightbox from "../Lightbox";
 import AppActionCreators from "../../actions/AppActionCreators";
@@ -72,6 +73,7 @@ export default class App extends React.Component {
           }
         </CSSTransitionGroup>
         <Footer />
+        { /* <Loader /> */ }
       </div>
     );
   }
