6d33d5bc1ef76bc3759acb7f13ebb9c1af5f2528
Christian de Botton <debotton@brooklynunited.com>
2015-06-30 15:36:34 -0400

    Tracking codes.

--- app/api/HTMLDocument.js
+++ app/api/HTMLDocument.js
@@ -46,6 +46,30 @@ export default class App extends React.Component {
         )) }
         <script src="//use.typekit.net/iey8vjp.js"></script>
         <script dangerouslySetInnerHTML={{__html: TYPE_KIT_EXEC }} />
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-20769653-1', 'auto');
+          ga('require', 'displayfeatures');
+          ga('require', 'linkid', 'linkid.js');
+          ga('send', 'pageview');
+        ` }} />
+        <noscript>
+          <iframe
+            src="//www.googletagmanager.com/ns.html?id=GTM-WKTC3B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }} />
+        </noscript>
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WKTC3B');
+        ` }} />
       </head>
       <body>
         <div
